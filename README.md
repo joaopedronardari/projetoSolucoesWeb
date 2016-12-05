d30d219b175f044829c761382da1649534daaab7
guilhermemt21 <guilherme.mt21@gmail.com>
2016-12-04 22:21:02 -0200

    Cadastro de imovel

--- README.md
+++ README.md
@@ -4,9 +4,14 @@ Repositorio Privado para o projeto de Solucoes Web
 
 # instruções
 1- NETBEANS -> Open Project -> Pasta do projeto -> ACH_2077
+
 2- Verificar o arquivo persistence.xml. Ele deve conter o usuário e a senha do seu mysql server
+
 3- Importar banco de dados. Crie no workbench um schema com o nome ACH_2077. Execute no terminal, dentro da pasta onde existe o dump, o seguinte comando mysql -u root -p ACH_2077 < ach_2077.dump.sql 
+
 4- Dê um run no netbeans. Acesse a página http://localhost:8080/ach_2077/imoveis.xhtml. Insira um novo imóvel, e veja se ele foi salvo no banco de dados
+
+5- https://showcase.bootsfaces.net/Examples/ -> Para montar novas páginas com HTML e CSS bootstrappados
 
 # troubleshooting
 1- Problemas com bibliotecas não encontradas: A referência pode estar quebrada. Botão direito no projeto -> Libraries, deleta as libs que estão com o "x" e reimporta as libs da pasta do projeto
--- ach_2077/src/java/model/Imovel.java
+++ ach_2077/src/java/model/Imovel.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.io.Serializable;
+import java.util.Date;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -23,8 +24,22 @@ public class Imovel implements Serializable{
     
     @Id @GeneratedValue
     private Long id_imovel;
-    
     private String titulo;
+    private String descricao;
+    private Double preco_venda;
+    private Double preco_aluguel;
+    private Double preco_condominio;
+    private String endereco;
+    private String bairro;
+    private Integer cep;
+    private String cidade;
+    private String estado;
+    private String tipo;
+    private Date data_anuncio;
+    private Integer area;
+    private Integer quartos;
+    private Integer vagas;
+    private Integer suites;
 
     public Long getId_imovel() {
         return id_imovel;
@@ -34,6 +49,66 @@ public class Imovel implements Serializable{
         return titulo;
     }
 
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public Double getPreco_venda() {
+        return preco_venda;
+    }
+
+    public Double getPreco_aluguel() {
+        return preco_aluguel;
+    }
+
+    public Double getPreco_condominio() {
+        return preco_condominio;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public Integer getCep() {
+        return cep;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public Date getData_anuncio() {
+        return data_anuncio;
+    }
+
+    public Integer getArea() {
+        return area;
+    }
+
+    public Integer getQuartos() {
+        return quartos;
+    }
+
+    public Integer getVagas() {
+        return vagas;
+    }
+
+    public Integer getSuites() {
+        return suites;
+    }
+
     public void setId_imovel(Long id_imovel) {
         this.id_imovel = id_imovel;
     }
@@ -41,6 +116,67 @@ public class Imovel implements Serializable{
     public void setTitulo(String titulo) {
         this.titulo = titulo;
     }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public void setPreco_venda(Double preco_venda) {
+        this.preco_venda = preco_venda;
+    }
+
+    public void setPreco_aluguel(Double preco_aluguel) {
+        this.preco_aluguel = preco_aluguel;
+    }
+
+    public void setPreco_condominio(Double preco_condominio) {
+        this.preco_condominio = preco_condominio;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public void setCep(Integer cep) {
+        this.cep = cep;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public void setData_anuncio(Date data_anuncio) {
+        this.data_anuncio = data_anuncio;
+    }
+
+    public void setArea(Integer area) {
+        this.area = area;
+    }
+
+    public void setQuartos(Integer quartos) {
+        this.quartos = quartos;
+    }
+
+    public void setVagas(Integer vagas) {
+        this.vagas = vagas;
+    }
+
+    public void setSuites(Integer suites) {
+        this.suites = suites;
+    }
+
     
     
 
